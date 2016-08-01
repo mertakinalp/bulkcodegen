71dc5d27fe7baa7e3e73070e74a0136cccbc020a
mertakinalp <mertakinalp@gmail.com>
2016-08-01 18:31:42 +0300

    commit
    commit

---
+++ BulkCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace BulkCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkUpper = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Location = new System.Drawing.Point(58, 16);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(309, 20);
+            this.txtPattern.TabIndex = 0;
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(58, 51);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chars";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Max";
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(58, 89);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 20);
+            this.txtMax.TabIndex = 4;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(431, 19);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 7;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Upper";
+            // 
+            // chkUpper
+            // 
+            this.chkUpper.AutoSize = true;
+            this.chkUpper.Location = new System.Drawing.Point(58, 125);
+            this.chkUpper.Name = "chkUpper";
+            this.chkUpper.Size = new System.Drawing.Size(15, 14);
+            this.chkUpper.TabIndex = 9;
+            this.chkUpper.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 149);
+            this.Controls.Add(this.chkUpper);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.txtPattern);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkUpper;
+    }
+}
+
---
+++ BulkCodeGenerator/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BulkCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPattern.Text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToLower();
+            txtLength.Text = "8";
+            txtMax.Text = "500";
+            chkUpper.Checked = true;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            List<string> arr = new List<string>();
+
+            
+
+            string Pat = "";
+            int Length = 0;
+            int Max = 0;
+
+            Pat = txtPattern.Text;
+
+            if (chkUpper.Checked)
+            {
+                Pat = Pat.ToUpper();
+            }
+
+            int.TryParse(txtLength.Text, out Length);
+            int.TryParse(txtMax.Text, out Max);
+            int maxSize = Length;
+            int minSize = Length;
+            char[] chars = new char[62];
+            string a;
+            a = Pat;
+            chars = a.ToCharArray();
+            int size = maxSize;
+            for (int i = 0; i < Max; i++)
+            {
+                byte[] data = new byte[1];
+                RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+                crypto.GetNonZeroBytes(data);
+                size = maxSize;
+                data = new byte[size];
+                crypto.GetNonZeroBytes(data);
+                StringBuilder result = new StringBuilder(size);
+                string re = "";
+                foreach (byte b in data)
+                {
+                    result.Append(chars[b % (chars.Length - 0)]);
+                }
+                re = result.ToString();
+                if (arr.Contains(re) == false)
+                {
+
+                    arr.Add(re);
+
+                }
+                else 
+                {
+                    i--;
+                    continue;
+                }
+                
+                
+            }
+
+            string strings = String.Join("\r\n", arr);
+            Clipboard.SetText(strings);
+            
+
+        }
+    }
+}
